582c79077d1991317be60ab0d3ea2d6ba914ea80
Robert Groot <rorogro@gmail.com>
2018-06-21 14:31:04 +0200

    Update

--- setup_ros1_pepper.bash
+++ setup_ros1_pepper.bash
@@ -4,8 +4,6 @@ export PYTHONHOME="/home/nao/.ros-root/Python-2.7.13"
 export PATH="${PYTHONHOME}/bin:${PATH}"
 export LD_LIBRARY_PATH="/home/nao/.ros-root/ros1_dependencies/lib:${PYTHONHOME}/lib:${LD_LIBRARY_PATH}"
 
-source /home/nao/.ros-root/ros1_inst/setup.bash
-
 export ROS_INTERFACE=eth0
 export LOCAL_IP=`ifconfig $ROS_INTERFACE | grep "inet " | cut -d ' ' -f 10`
 export ROS_MASTER_IP=169.254.114.121
@@ -14,3 +12,7 @@ export ROS_HOSTNAME=$LOCAL_IP
 export ROS_IP=$LOCAL_IP
 
 export NAMESPACE=Pepper0
+
+alias pepper_run_ros='roslaunch naoqi_driver naoqi_driver.launch roscore_ip:=$ROS_MASTER_IP network_interface:=$ROS_INTERFACE namespace:=$NAMESPACE'
+
+source /home/nao/.ros-root/ros1_inst/setup.bash
